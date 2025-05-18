35f7aacf471aa48feee0e1312acc6a4c740efb2d
1Zeref <hieu.nguyenchi.hcth@gmail.com>
2025-05-18 05:18:07 -0700

    UI

--- get-informations.ps1
+++ get-informations.ps1
@@ -1,4 +1,6 @@
-Write-Host "=== CHỌN SCRIPT ==="
+Write-Host "=============================="
+Write-Host "Thông tin máy tính"
+Write-Host "=============================="
 # Lấy thông tin BIOS để lấy Serial Number
 $bios = Get-WmiObject -Class Win32_BIOS
 
