bc4aac1df6f34b5727fa309a2d3e64e966301488
1Zeref <hieu.nguyenchi.hcth@gmail.com>
2025-05-18 04:50:53 -0700

    Add files via upload

---
+++ get-informations.ps1
@@ -0,0 +1,8 @@
+# Lấy thông tin BIOS để lấy Serial Number
+$bios = Get-WmiObject -Class Win32_BIOS
+
+# Hiển thị Serial Number
+Write-Host "Serial Number: $($bios.SerialNumber)"
+
+# Dừng lại, yêu cầu người dùng nhấn Enter để thoát
+Read-Host -Prompt "Nhấn Enter để thoát"
