90f6d767e298f848191413553a7d8329ba634592
1Zeref <hieu.nguyenchi.hcth@gmail.com>
2025-05-18 05:06:49 -0700

    fixed

--- run-tool.bat
+++ run-tool.bat
@@ -10,7 +10,7 @@ echo ===========================
 set /p choice="Nhap lua chon (1-1): "
 
 :: Gán URL tương ứng
-if "%choice%"=="1" set "ps1url=https://raw.githubusercontent.com/1Zeref/setup-laptop/main/get-informations.ps1"
+if "%choice%"=="1" set "ps1url=https://raw.githubusercontent.com/1Zeref/setup-laptop/refs/heads/main/get-informations.ps1"
 
 :: Kiểm tra quyền Admin
 NET SESSION >nul 2>&1
