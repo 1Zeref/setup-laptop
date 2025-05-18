f2bd2eb10fae53e4f896aa4515720466cf5d6056
1Zeref <hieu.nguyenchi.hcth@gmail.com>
2025-05-18 05:00:15 -0700

    update

---
+++ run-tool.bat
@@ -0,0 +1,27 @@
+@echo off
+title Chạy Script PS1 từ URL
+color 0A
+
+:: Menu chọn
+echo ===========================
+echo Chon script ban muon chay:
+echo 1. Lấy Serial
+echo ===========================
+set /p choice="Nhap lua chon (1-1): "
+
+:: Tùy chọn URL tương ứng
+if "%choice%"=="1" set ps1url=1Zeref/setup-laptop/get-informations.ps1
+
+:: Kiểm tra quyền Admin
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    echo Dang yeu cau quyen Administrator...
+    powershell -Command "Start-Process '%~f0' -Verb runAs"
+    exit /b
+)
+
+:: Tải và chạy script từ URL
+powershell -NoProfile -ExecutionPolicy Bypass -Command "irm 'raw.githubusercontent.com/%ps1url%' | powershell -NoProfile -ExecutionPolicy Bypass -"
+
+:: Dừng để xem kết quả
+pause
